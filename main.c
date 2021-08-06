0109a41905a16e332d0125bc2849bb92c25e719e
nonoaa <nonoaa1@naver.com>
2021-08-06 19:34:02 +0900

    init

--- main.c
+++ main.c
@@ -1,18 +1,70 @@
 #include <mlx.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int        main(void)
+# define KEY_ESC 53
+#define KEY_W 13
+#define KEY_A 0
+#define KEY_S 1
+#define KEY_D 2
+
+typedef struct	s_mlx
 {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win;
 	void	*img;
+	int		cnt;
+}				t_mlx;
+
+typedef	struct s_player
+{
+	int		x;
+	int		y;
+	int		cnt;
+}				t_player;
+
+int	get_next_line(int fd, char **line);
+
+int x = 0;
+int y = 0;
+
+int		deal_key(int keycode, t_mlx *mlxs)
+{
+		if (keycode == KEY_ESC)
+				exit(0);
+		else if (keycode == KEY_W)
+				;
+		else if (keycode == KEY_A)
+				;
+		else if (keycode == KEY_S)
+		{
+				y += 50;
+				mlx_put_image_to_window(mlxs->mlx, mlxs->win, mlxs->img, x, y);
+		}
+		else if (keycode == KEY_D)
+		{
+				x += 50;
+				mlx_put_image_to_window(mlxs->mlx, mlxs->win, mlxs->img, x, y);
+		}
+		(mlxs->cnt)++;
+		printf("cnt:%d\n", mlxs->cnt);
+		return (0);
+}
+
+int		main(int argc, char *argv[])
+{
+	t_mlx	mlxs;
 
 	int	img_width;
 	int	img_height;
+	
+	mlxs.cnt = 0;
+	mlxs.mlx = mlx_init();
+	mlxs.win = mlx_new_window(mlxs.mlx, 1200, 600, "so_long");
+	mlxs.img = mlx_xpm_file_to_image(mlxs.mlx, "./images/tile.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlxs.mlx, mlxs.win, mlxs.img ,0, 0);
+	mlx_key_hook(mlxs.win, &deal_key, &mlxs);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1000, 800, "so_long");
-	img = mlx_png_file_to_image(mlx, "./images/map.png", &img_width, &img_height);
-	mlx_put_image_to_window(mlx, mlx_win, img ,720, 480);
-	mlx_loop(mlx);
+	mlx_loop(mlxs.mlx);
 	return (0);
 }
